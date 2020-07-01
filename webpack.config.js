c333b4df5fbd8ab4cc6c192720c5ec33bd7cb76f
srg <skabass@yandex.ru>
2020-07-01 21:21:43 +0300

    feat: add setting for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,77 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-  entry: `./source/index.js`
+  entry: [
+    './source/index.js', 
+    './source/sass/style.scss'
+  ],
+  output: {
+    filename: './js/script.js',
+  },
+  devtool: 'source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        include: path.join(__dirname, 'source/sass'),
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+              url: false,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { 
+          from: '*.(woff|woff2)', 
+          to: './fonts/',
+          context: 'source/fonts/' 
+        },
+        { 
+          from: '**/*.(svg|png|jpg|jpeg)', 
+          to: './img/',
+          context: './source/img/'
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: './css/style.css'
+    }),
+    new HtmlWebpackPlugin({
+      template: './source/index.html'
+    }),
+  ],
 }
